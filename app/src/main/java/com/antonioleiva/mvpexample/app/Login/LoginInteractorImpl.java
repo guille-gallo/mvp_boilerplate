fdc427ef77a68e4d551f799f708cd3f2460882e5
guille-gallo <guillermo.gallo@globant.com>
2017-07-27 12:47:15 -0300

    login api request done, jsonarray response parse partially done

--- app/src/main/java/com/antonioleiva/mvpexample/app/Login/LoginInteractorImpl.java
+++ app/src/main/java/com/antonioleiva/mvpexample/app/Login/LoginInteractorImpl.java
@@ -1,7 +1,6 @@
 package com.antonioleiva.mvpexample.app.Login;
 
 import android.content.Context;
-import android.os.Handler;
 import android.text.TextUtils;
 
 import com.android.volley.Request;
@@ -13,56 +12,59 @@ import com.antonioleiva.mvpexample.app.main.VolleyClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.util.ArrayList;
+
+
 public class LoginInteractorImpl implements LoginInteractor {
+
+    ArrayList datos = new ArrayList();
 
     @Override
     public void login(final String username, final String password, final OnLoginFinishedListener listener, final Context context) {
-        // Mock login. I'm creating a handler to delay the answer a couple of seconds
-        new Handler().postDelayed(new Runnable() {
-            @Override public void run() {
-                boolean error = false;
-                if (TextUtils.isEmpty(username)){
-                    listener.onUsernameError();
-                    error = true;
-                    return;
-                }
-                if (TextUtils.isEmpty(password)){
-                    listener.onPasswordError();
-                    error = true;
-                    return;
-                }
-                if (!error){
-                    //listener.onSuccess();
-                    String url = "https://jsonplaceholder.typicode.com/posts";
-                    JsonArrayRequest jsArrayRequest = new JsonArrayRequest (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
-                        @Override
-                        public void onResponse(JSONArray response) {
-                            for (int i = 0; i < response.length(); i++) {
-                                try {
-                                    String userName = response.getJSONObject(i).getString("userName");
-                                    String userFirstName = response.getJSONObject(i).getString("userFirstName");
-                                    String userLastName = response.getJSONObject(i).getString("userLastName");
+        boolean error = false;
+        if (TextUtils.isEmpty(username)){
+            listener.onUsernameError();
+            error = true;
+            return;
+        }
+        if (TextUtils.isEmpty(password)){
+            listener.onPasswordError();
+            error = true;
+            return;
+        }
+        if (!error){
 
-                                    //datos.add(new UserDetail(userName, userFirstName, userLastName));
+            String url = "https://api.myjson.com/bins/zuj5h";
+            JsonArrayRequest jsArrayRequest = new JsonArrayRequest (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                @Override
+                public void onResponse(JSONArray response) {
+                    for (int i = 0; i < response.length(); i++) {
+                        try {
+                            String userFirstName = response.getJSONObject(i).getString("First Name");
+                            String userLastName = response.getJSONObject(i).getString("Last Name");
+                            String userID = response.getJSONObject(i).getString("ID");
 
-                                } catch (JSONException ex) {
-                                    System.out.println(ex);
-                                }
-                            }
-                            /*adapter.notifyDataSetChanged();
-                            hideProgressBar();*/
+                            /*
+                            * popular List/Arraylist, mandarlo a listener.onSuccess();
+                            * */
+                            //datos.add(userFirstName, userLastName, userID);
+                            //listener.onSuccess();
+                        } catch (JSONException ex) {
+                            System.out.println(ex);
                         }
-                    }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            String hola = error.toString();
-                            // TODO Auto-generated method stub
-                        }
-                    });
-                    // Access the RequestQueue through your singleton class.
-                    VolleyClient.getInstance(context).addToRequestQueue(jsArrayRequest);
+                    }
+                    /*adapter.notifyDataSetChanged();
+                    hideProgressBar();*/
                 }
-            }
-        }, 2000);
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    String hola = error.toString();
+                    // TODO Auto-generated method stub
+                }
+            });
+            // Access the RequestQueue through your singleton class.
+            VolleyClient.getInstance(context).addToRequestQueue(jsArrayRequest);
+        }
     }
 }
